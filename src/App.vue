2effad4c9fbae5e13920a58d8ea7b049755fee3a
ryanwolhuter <dev@ryanwolhuter.com>
2020-12-05 08:46:04 +0200

    Add slide component.

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,19 @@
 <template>
-  <Particles
-    id="tsparticles"
-    :options="{
-      fpsLimit: 60,
-      particles: {
-        color: {
-          value: '#000000',
-        },
-        links: {
-          color: '#000000',
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        collisions: {
-          enable: true,
-        },
-        move: {
-          direction: 'none',
-          enable: true,
-          outMode: 'bounce',
-          random: false,
-          speed: 6,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-            value_area: 800,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: 'circle',
-        },
-        size: {
-          random: true,
-          value: 5,
-        },
-      },
-      detectRetina: true,
-    }"
-  />
+  <Slide>
+    <h1>Heading</h1>
+    <p>Text</p>
+  </Slide>
 </template>
 
 <script>
+  import Slide from '@/components/Slide.vue'
+
 
 export default {
+
   name: 'App',
   components: {
+    Slide
   }
 }
 </script>
---
+++ src/components/Slide.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="slide">
+    <Particles id="tsparticles" :options="particlesOptions" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slide',
+  data() {
+    return {
+      particlesOptions: {
+      fpsLimit: 60,
+      particles: {
+        color: {
+          value: '#000000',
+        },
+        links: {
+          color: '#000000',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outMode: 'bounce',
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+          value: 20,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: 'circle',
+        },
+        size: {
+          random: true,
+          value: 5,
+        },
+      },
+      detectRetina: true,
+    }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#tsparticles {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+.slide {
+  height: 100vh;
+}
+</style>
